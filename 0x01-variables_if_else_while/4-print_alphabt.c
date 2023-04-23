f11d7c5b62ea5cdbffdee2b7411264aa1decb43d
Nomthy31 <nlmabena2015@gmail.com>
2023-04-23 14:22:36 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		if (x == 101 || x == 113)
+		{
+			x++;
+			continue;
+		}
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
