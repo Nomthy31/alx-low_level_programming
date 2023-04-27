c17a816a27a7ce720c886f9a384808104c9c1751
Nomthy31 <nlmabena2015@gmail.com>
2023-04-27 13:43:07 -0700

    task done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	long y = 1;
+	long z = 2;
+
+	while (x < 50)
+	{
+		if (x == 0)
+			printf("%ld", y);
+		else if (x == 1)
+			printf("%ld", z);
+		else
+		{
+			z += y;
+			y = x - y;
+			printf(", %ld", z);
+		}
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
