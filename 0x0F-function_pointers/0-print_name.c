85a207b7bb88087d79b2e80d594e52f40369d450
Nomthy31 <nlmabena2015@gmail.com>
2023-05-24 14:28:09 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "FUNCTION_POINTERS.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: given name
+ * @f: function of name
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
